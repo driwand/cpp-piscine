ca638671c2aa37a3cbe3475548b2ae5cf5f18906
driwand <44057801+driwand@users.noreply.github.com>
2021-12-15 19:51:59 +0100

    update

--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -3,15 +3,36 @@
 
 int main() {
 	try {
-		Array<std::string> arr(1);
-		arr[0] = 1;
-		std::cout << arr[0] << std::endl;
-		std::cout << "Array size: " << arr.size() << std::endl;
+		std::cout << "- - - - - Array of ints - - - - -\n";
+		int size = 9;
+		Array<int> arr(size);
 
-		// Array<std::string> ee;
-		// ee = arr;
-		// ee[0] = "hello";
-		// std::cout << "Array size: " << ee[0] << std::endl;
+		for (int i = 0; i < size; i++) {
+			arr[i] = i + 1;
+		}
+
+		std::cout << "Array size: " << arr.size() << std::endl;
+		for (int i = 0; i < size; i++) {
+			std::cout << "arr[" << i << "] : " << arr[i] << std::endl;
+		}
+
+		std::cout << "- - - - - Copy of ints - - - - -\n";
+		Array<int> arrCopy(arr);
+		for (int i = 0; i < size; i++) {
+			std::cout << "arrCopy[" << i << "] : " << arr[i] << std::endl;
+		}
+
+		std::cout << "\n- - - - - Array of strings - - - - -\n";
+		Array<std::string> ee(1);
+		ee[0] = "hello";
+		std::cout << "Array size: " << ee.size() << std::endl;
+		std::cout << "ee[0] : " << ee[0] << std::endl;
+
+		std::cout << "\n- - - - - Array of strings with an error - - - - -\n";
+		Array<std::string> strarr;
+		strarr[0] = "hello";
+		std::cout << "Array size: " << strarr[0] << std::endl;
+
 	} catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
