082b380df917a1c471a925e543c43d13bfa73dde
driwand <44057801+driwand@users.noreply.github.com>
2021-12-16 15:47:08 +0100

    update

--- day07/ex01/main.cpp
+++ day07/ex01/main.cpp
@@ -1,35 +1,23 @@
 #include "iter.hpp"
 #include <cstring>
 
-int printElm(int i) {
+void printElm(int i) {
 	std::cout << i << std::endl;
-	return i;
 }
 
-int incrementElms(int i) {
-	return ++i;
-}
-
-char printElmChar(char elm) {
+void printElmStr(std::string elm) {
 	std::cout << elm << std::endl;
-	return elm;
-}
-
-char toUpper(char elm) {
-	return ++elm;
 }
 
 int main() {
 	std::cout << "- - - - - - - int - - - - - - -\n";
 
 	int arrInt[] = { 16, 2, 77, 40, 12071 };
-	iter(arrInt, 5, incrementElms);
 	iter(arrInt, 5, printElm);
 
 	std::cout << "- - - - - - - char - - - - - - -\n";
 
-	char arrChar[] = { 'a', 'b', 'c' };
-	iter(arrChar, 3, toUpper);
-	iter(arrChar, 3, printElmChar);
+	std::string arrChar[] = { "abc", "def", "ghi" };
+	iter(arrChar, 3, printElmStr);
 	return 0;
 }
